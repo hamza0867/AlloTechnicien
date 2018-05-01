162337d639f4f6a12a2ae86ec1dbd67e7e978ee9
hamza <hamza0867@gmail.com>
2018-05-01 10:24:25 +0100

    commit 2

--- app/src/main/java/com/example/hamza/allotechnicien/MainActivity.java
+++ app/src/main/java/com/example/hamza/allotechnicien/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends BaseActivity {
         contentView = getLayoutInflater().inflate(R.layout.subscribe_activity_layout, null);
         contentView.setLayoutParams(layoutParams);
         activityLayout.addView(contentView);
-        // :-)
     }
 }
